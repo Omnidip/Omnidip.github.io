89f35a786b6202f2ca51e562ef9c47f1d4172cf7
Omnidip <joey_roorda@hotmail.com>
2018-07-24 16:50:53 -0400

    navigation tweaks

--- js/main.js
+++ js/main.js
@@ -16,20 +16,11 @@ function load_post(post){
                     <p> Sorry, I can't seem to find that post. </p>
                 `;
             }
+            toggle_blog(true);
         }
     };
     xhttp.open("GET", "/post/" + encodeURIComponent(post) + ".html", true);
     xhttp.send();
-}
-
-function load_page(){
-    search_parameters = new URLSearchParams(window.location.search);
-    if(search_parameters.has('p')){
-        load_post(search_parameters.get('p'));
-    }else{
-        history.pushState({}, "", "index.html?p="+latest_post);
-        load_post(latest_post);
-    }
 }
 
 function get_post(inc){
@@ -46,5 +37,69 @@ function get_post(inc){
     }
 }
 
-window.onpopstate = function(event) {load_page();};
-window.onload = load_page();
+function load_page(page){
+    page_body = document.getElementById("page_content");
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            if(this.status == 200){
+                page_body.innerHTML = this.responseText;
+            }else{
+                page_body.innerHTML = `
+                    <h1> 404 - Not found </h1>
+                    <p> Sorry, I can't seem to find that page. </p>
+                `;
+            }
+            toggle_blog(false);
+        }
+    };
+    xhttp.open("GET", "/page/" + encodeURIComponent(page) + ".html", true);
+    xhttp.send();
+}
+
+function toggle_blog(b){
+    blog_body = document.getElementById("blog_content");
+    page_body = document.getElementById("page_content");
+    if(b){
+        page_body.innerHTML = "";
+        page_body.style.display = "none";
+        blog_body.style.display = "block";
+    }else{
+        post_body = document.getElementById("post");
+        post_body.innerHTML = "";
+        blog_body.style.display = "none";
+        page_body.style.display = "block";
+    }
+}
+
+function init_page(){
+    search_parameters = new URLSearchParams(window.location.search);
+    if(search_parameters.has('page')){
+        load_page(search_parameters.get('page'));
+    }else{
+        if(search_parameters.has('p')){
+            load_post(search_parameters.get('p'));
+        }else{
+            history.pushState({}, "", "index.html?p="+latest_post);
+            load_post(latest_post);
+        }
+    }
+}
+
+function load_about(){
+    history.pushState({}, "", "index.html?page=about");
+    init_page();
+}
+
+function load_archive(){
+    history.pushState({}, "", "index.html?page=archive");
+    init_page();
+}
+
+function load_blog(){
+    history.pushState({}, "", "index.html");
+    init_page();
+}
+
+window.onpopstate = function(event) {init_page();};
+window.onload = init_page();
