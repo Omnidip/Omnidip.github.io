ecb530eae5acbe606873cf345d888c8e72a585b2
Omnidip <joey_roorda@hotmail.com>
2018-07-24 14:39:08 -0400

    functionality

--- js/main.js
+++ js/main.js
@@ -1,77 +1,50 @@
-//Credits
-console.log("Logo font is 'Dragon is coming' by Maelle.K | Thomas Boucherie on DaFont.com");
 
-//----------------------//
-//----LOGO ANIMATION----//
-//----------------------//
+// --------------- //
+latest_post = 1;
+// --------------- //
 
-logo_animation_canvas = document.getElementById("logo_animation");
-
-lac_w = 1000;
-lac_h = 200;
-
-//center
-lac_wc = lac_w/2.0;
-lac_hc = lac_h/2.0;
-
-//padding
-lac_pradw = lac_wc - 5;
-lac_pradh = lac_hc - 10;
-
-//offset
-lac_offx = -450;
-lac_offy = -80;
-
-lac_context = logo_animation_canvas.getContext("2d");
-
-logo_n_arms     = 12;
-logo_p_per_arm  = 30;
-logo_p_base     = 10;//px
-logo_p_varience = 20;//px
-logo_step       = 0;
-logo_tilt       = 1.45;//rad
-logo_particles  = [];
-
-//generate the galaxy particles
-for (a=logo_n_arms-1; a>=0; a--) {//reverse order
-    for (p=0; p<logo_p_per_arm; p++) {
-        logo_particles.push([a, Math.random(), Math.random(), Math.random(), Math.round(Math.random()-0.2)])//arm, pos, width, height, color
-    }
-}
-
-//logo_particles.sort(function(a,b){return b[1] - a[1]});
-
-function draw_particles(){
-    //clear the canvas for drawing
-    lac_context.clearRect(0, 0, lac_w, lac_h);
-
-    for (p=0; p<logo_particles.length; p++) {
-        part = logo_particles[p];
-
-        if(part[4] == 0){
-            lac_context.fillStyle = "#3D3C4D";
-        }else{
-            lac_context.fillStyle = "#A65C57";
+function load_post(post){
+    post_body = document.getElementById("post");
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            if(this.status == 200){
+                post_body.innerHTML = this.responseText;
+            }else{
+                post_body.innerHTML = `
+                    <h1> 404 - Not found </h1>
+                    <p> Sorry, I can't seem to find that post. </p>
+                `;
+            }
         }
+    };
+    xhttp.open("GET", "/post/" + encodeURIComponent(post) + ".html", true);
+    xhttp.send();
+}
 
-        ang = ((Math.PI*2.0)/logo_n_arms) * part[0] + ((Math.PI/1.8) * part[1]) - (logo_step/500.0);
-
-        xoff = (Math.sin(ang) * part[1]*lac_pradw);
-        yoff = (Math.cos(ang) * part[1]*lac_pradh);
-
-        px = lac_wc + xoff*Math.sin(logo_tilt) + yoff*Math.cos(logo_tilt);
-        py = lac_hc + xoff*Math.cos(logo_tilt) + yoff*Math.sin(logo_tilt);
-
-        lac_context.fillRect(
-            px + lac_offx,
-            py + lac_offy,
-            part[2]*logo_p_varience + logo_p_base,
-            part[3]*logo_p_varience + logo_p_base
-        );
+function load_page(){
+    search_parameters = new URLSearchParams(window.location.search);
+    if(search_parameters.has('p')){
+        load_post(search_parameters.get('p'));
+    }else{
+        history.pushState({}, "", "index.html?p="+latest_post);
+        load_post(latest_post);
     }
 }
 
-setInterval(function(){
-    logo_step++;
-    draw_particles();
-},50);
+function get_post(inc){
+    search_parameters = new URLSearchParams(window.location.search);
+    this_post = latest_post;
+    if(search_parameters.has('p')){
+        this_post = parseInt(search_parameters.get('p'));
+    }
+
+    new_post = this_post + inc;
+    if( new_post <= latest_post && new_post >= 1){
+        history.pushState({}, "", "index.html?p="+new_post);
+        load_post(new_post);
+    }
+}
+
+window.onpopstate = function(event) {load_page();};
+window.onload = load_page();
